456af1f7ba14bb466606ba37a651dd6868dac070
Nate <nathan.harris@force5solutions.com>
2021-09-22 14:30:15 -0600

    Initial commit

---
+++ logride/_info.py
@@ -0,0 +1,21 @@
+__version__ = '0.1.0'
+
+__title__ = "logride"
+__author__ = 'Nate Harris'
+__author_email__ = 'n8gr8gbln@gmail.com'
+__copyright__ = "Copyright © 2021 - Nate Harris"
+__license__ = 'GNU General Public License v3 (GPLv3)'
+__docs__ = "https://logride.readthedocs.io/en/latest"
+__description__ = "Interact with LogRide and ThemeParkDatabase's API"
+__keywords__ = [
+        'logride',
+        'theme park',
+        'rides',
+        'roller coaster',
+        'water park',
+        'amusement park',
+        'data',
+        'API'
+    ]
+
+__analytics_id__ = ""
